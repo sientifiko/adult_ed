988a0afb4b0cac24868984ff74c98dcd5403f9f1
el-racional <esteban.carreno.c@gmail.com>
2019-07-11 22:32:00 -0400

    según parece rvest dejó de trabajar con gscholar, considerar RSelenium

--- google_scholar_scraping.R
+++ google_scholar_scraping.R
@@ -15,24 +15,40 @@ google_scholar_df <- function(url, iterations){
   page <- read_html(url)
   closeAllConnections()
   
+  p <- 1
   titles <- c()
   links <- c()
-  
+  url <- str_replace(url, "0", "TO_REPLACE" )
   # this loop fills the titles and links vector
   for(i in 1:iterations){
     
-  }
+    to_replace <- paste(p, "0", sep = "")
+    
+    titles <- append(titles, html_text(html_nodes(page, "h3.gs_rt" )))
+    links <- append(links, html_attr(html_nodes(page,"h3.gs_rt > a"), "href"))
+    
+    page <- read_html( str_replace(url, "TO_REPLACE", to_replace )  )
+    closeAllConnections()
+    
+    p <- p + 1
+  }# end of loop
   
+  titles <- clean_vector(titles)
   
-  titles <- html_text(html_nodes(page, "h3.gs_rt" ))
-  links <- html_attr(html_nodes(page,"h3.gs_rt > a"), "href")
-  
-  df <- data.frame(links, titles)
+  return( data.frame(links, titles) )
   
 }# end of google_scholar_df
 
 
-clean_text(titles){
+clean_vector <- function(titles){
+  for (i in 1:length(titles)) {
+    if(str_detect(titles, "CITAS")[i]){
+      titles[i] <- "delete"
+    }
+  }
+  titles <- setdiff(titles, y="delete")
+  
+  # remove unnecesary characters from titles
   titles <- gsub("PDF","", titles)
   titles <- gsub("HTML", "",titles)
   titles <- gsub("\\[|\\]", "", titles)
@@ -40,22 +56,8 @@ clean_text(titles){
   return(titles)
 } # end of clean_text
 
-next_link <- function(url, i){
-  # this functions creates the new link to
-  # navigate the current query
-  if(!is.na( str_locate(url, "TO_REPLACE")[1] ) ){
-    
-  } else {
-    
-    url <- str_replace(url, "0", "TO_REPLACE" )
-    next_link(url,i)
-  }
+
   
-  
-}# end of next_link
-
-
-
 #============================ SCHOLAR.GOOGLE.CL SEARCH Nº1 ============================
 #                        Search for "deserción" & "Chile"
 #                          Date of search 10-07-2019
@@ -63,15 +65,54 @@ next_link <- function(url, i){
 url <- "https://scholar.google.cl/scholar?start=0&q=deserci%C3%B3n+AND+Chile&hl=es&as_sdt=1,5"
 
 
+
 page <- read_html(url)
 closeAllConnections()
 
-titles <- html_text(html_nodes(page, "h3.gs_rt" ))
-links <- html_attr(html_nodes(page,"h3.gs_rt > a"), "href")
+p <- 1
+titles <- c()
+links <- c()
+url <- str_replace(url, "0", "TO_REPLACE" )
+# this loop fills the titles and links vector
 
-str_locate(url, "0")[1]
-is.na( str_locate(url, "TO_REPLACE")[1] )
 
-url2 <- str_replace(url, "0", "TO_REPLACE" )
+html_text( html_nodes(page, "div.gs_ri" ) )
+titles
 
-str_replace(url2, "TO_REPLACE", "40")
+for(i in 1:iterations){
+  
+  to_replace <- paste(p, "0", sep = "")
+  
+  titles <- append(titles, html_text(html_nodes(page, "h3.gs_rt" )))
+  links <- append(links, html_attr(html_nodes(page,"h3.gs_rt > a"), "href"))
+  
+  page <- read_html( str_replace(url, "TO_REPLACE", to_replace )  )
+  closeAllConnections()
+  
+  p <- p + 1
+}# end of loop
+
+titles <- clean_vector(titles)
+
+
+
+
+
+
+
+
+
+
+
+
+
+df.gscholar_1 <- google_scholar_df(url)
+
+df <- google_scholar_df(url)
+
+
+
+
+
+
+
