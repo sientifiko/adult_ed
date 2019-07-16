c72f5f7561df8f6c36a32719b4261c8c39b4eb15
el-racional <esteban.carreno.c@gmail.com>
2019-07-16 15:52:03 -0400

    logre hacer andar función gscholar

--- google_scholar_scraping.R
+++ google_scholar_scraping.R
@@ -1,6 +1,8 @@
 
 library(curl); library(xml2); library(rvest);library(stringr)
 
+install.packages("RSelenium")
+
 #============================ General function definition ============================
 
 
@@ -42,76 +44,47 @@ google_scholar_df <- function(url, iterations){
 
 clean_vector <- function(titles){
   for (i in 1:length(titles)) {
-    if(str_detect(titles, "CITAS")[i]){
-      titles[i] <- "delete"
+    
+    val <- str_detect(titles, "CITAS")[i]
+    if(is.na(val||missing(val))){
+      val <- T
+    }
+    if( val ){
+      print(paste("removing: ", titles[i]))
+      titles[i] <- "to_delete"
     }
   }
-  titles <- setdiff(titles, y="delete")
+  if(length(which(titles == "to_delete")>0)){
+    titles <- titles[-c(which(titles == "to_delete"))]
+  }
   
   # remove unnecesary characters from titles
   titles <- gsub("PDF","", titles)
   titles <- gsub("HTML", "",titles)
   titles <- gsub("\\[|\\]", "", titles)
+  titles <- gsub("LIBROB", "",titles)
   
   return(titles)
-} # end of clean_text
+} # end of clean_vector
 
 
   
 #============================ SCHOLAR.GOOGLE.CL SEARCH Nº1 ============================
 #                        Search for "deserción" & "Chile"
-#                          Date of search 10-07-2019
+#                          Date of search 16-07-2019
 
 url <- "https://scholar.google.cl/scholar?start=0&q=deserci%C3%B3n+AND+Chile&hl=es&as_sdt=1,5"
 
-
-
-page <- read_html(url)
-closeAllConnections()
-
-p <- 1
-titles <- c()
-links <- c()
-url <- str_replace(url, "0", "TO_REPLACE" )
-# this loop fills the titles and links vector
-
-
-html_text( html_nodes(page, "div.gs_ri" ) )
-titles
-
-for(i in 1:iterations){
-  
-  to_replace <- paste(p, "0", sep = "")
-  
-  titles <- append(titles, html_text(html_nodes(page, "h3.gs_rt" )))
-  links <- append(links, html_attr(html_nodes(page,"h3.gs_rt > a"), "href"))
-  
-  page <- read_html( str_replace(url, "TO_REPLACE", to_replace )  )
-  closeAllConnections()
-  
-  p <- p + 1
-}# end of loop
-
-titles <- clean_vector(titles)
-
-
-
-
-
-
-
-
-
-
-
-
-
 df.gscholar_1 <- google_scholar_df(url)
 
-df <- google_scholar_df(url)
 
+#============================ SCHOLAR.GOOGLE.CL SEARCH Nº2 ============================
+#                          Search for "fracaso escolar" & "Chile"
+#                              Date of search 16-07-2019
 
+url <- "https://scholar.google.cl/scholar?start=0&q=%22fracaso+escolar%22+AND+Chile&hl=en&as_sdt=1,5"
 
+df.gscholar_2 <- google_scholar_df(url)
 
 
 
